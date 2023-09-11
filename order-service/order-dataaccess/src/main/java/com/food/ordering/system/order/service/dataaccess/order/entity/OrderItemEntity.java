e336aa2c3b3878825862d759496c0281e0a62e39
dongguk Ryu <ludwig.ryu@gmail.com>
2023-09-11 22:53:41 +0900

    implement JPA Entities

---
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/order/entity/OrderItemEntity.java
@@ -0,0 +1,44 @@
+package com.food.ordering.system.order.service.dataaccess.order.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name="order_items")
+@IdClass(OrderItemEntityId.class)
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItemEntity {
+    @Id
+    private Long id;
+    @Id
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "ORDER_ID")
+    private OrderEntity order;
+
+    private UUID productId;
+    private BigDecimal price;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemEntity that = (OrderItemEntity) o;
+        return Objects.equals(id, that.id) && Objects.equals(order, that.order);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, order);
+    }
+
+    private Integer quantity;
+    private BigDecimal subTotal;
+}
